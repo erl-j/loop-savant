fc2e94882781cc0f0f8da687aa63f762187750ab
Nicolas  Jonason <nicolasjonason@Nicolass-MacBook-Pro.local>
2023-01-14 21:15:41 +0100

    wasm onnx model works

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Roll from './roll.js';
 import React from 'react';
+import ModelTest from './ModelTest.js';
 
 function App() {
 
   const [isOn, setIsOn] = React.useState(false);
   return (
     <div className="App">
+      <ModelTest />
       <button onClick={() => setIsOn(!isOn)}>{isOn ? "Stop" : "Start"}</button>
-      {isOn ? <Roll /> : null}
+      {/* {isOn ? <Roll /> : null} */}
     </div>
   );
 }
